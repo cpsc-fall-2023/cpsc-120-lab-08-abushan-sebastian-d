ef563958201650f98c354cc2f41061a092d347f0
Sebastian Delgadillo <cheb@csu.fullerton.edu>
2023-11-07 23:43:38 -0800

    Sixth commit

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -19,7 +19,8 @@ int main(int argc, char* argv[]) {
   std::string bread = arguments[2];
   std::string condiment = arguments[3];
 
-  std::cout << "Your order: A " << protein << " sandwich on " << bread << " with " << condiment << ".";
+  std::cout << "Your order: A " << protein << " sandwich on " << bread
+            << " with " << condiment << ".";
 
   return 0;
 }
