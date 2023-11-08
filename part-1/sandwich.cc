96b8a36c114a19969798cce106567cad674c03c7
Sebastian Delgadillo <cheb@csu.fullerton.edu>
2023-11-07 23:27:36 -0800

    First commit

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Sebastian Delgadillo
+// cheb@csu.fullerton.edu
+// @sdelgadiillo
+// Partners: @84u5ro
 
 #include <iostream>
 #include <string>
@@ -9,15 +12,23 @@ int main(int argc, char* argv[]) {
 
   // TODO: Validate that the number of arguments is correct.
   // If not, print an error message and return a non-zero value.
+  if (arguments.size() != 4) {
+    std::cerr << "Usage: " << argv[0] << " <PROTEIN> <BREAD> <CONDIMENT>" << std::endl;
+    return 1;
+  }
 
   // TODO: Declare three std::string variables to hold the
   // protein, bread, and condiment input.
   // Initialize each variable with an element of the arguments vector
   // declared above.
+  std::string protein = arguments[1];
+  std::string bread = arguments[2];
+  std::string condiment = arguments[3];
 
   // TODO: Use cout to print output following the pattern
   // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
   // on its own line.
+  std::cout << "Your order: A " << protein << " sandwich on " << bread << " with " << condiment << "." << std::endl;
 
   return 0;
 }
